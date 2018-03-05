7f2499cfbc542c672af2a81f8f0be1661e0ee5f8
Anduin-Zhu <932186976@qq.com>
2018-03-05 17:39:00 +0800

    封装一个断言

--- src/test/interface/test_baidu_http.py
+++ src/test/interface/test_baidu_http.py
@@ -7,6 +7,7 @@ from utils.config import Config,REPORT_PATH
 from utils.client import HTTPClient
 from utils.log import logger
 from utils.HTMLTestRunner_PY3 import HTMLTestRunner
+from  utils.assertion import assertHTTPCode
 
 class TestBaiDuHttp(unittest.TestCase):
     URL = Config().get('URL')
@@ -17,11 +18,13 @@ class TestBaiDuHttp(unittest.TestCase):
     def test_baidu_http(self):
         res = self.client.send()
         logger.debug(res.text)
+        #添加断言
+        assertHTTPCode(res,[400])
         self.assertIn('百度一下，你就知道',res.text)
 
 if __name__ == '__main__':
     now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
     report = REPORT_PATH + r'\\' + now + 'report.html'
-    f = open(report, 'wb')
+    f = open(report, 'wb')#没有生成测试报告是因为没有以文件方式执行
     runner = HTMLTestRunner(f, verbosity=2, title=u'百度测试报告', description=u'用例执行情况')
     runner.run(TestBaiDuHttp('test_baidu_http'))
